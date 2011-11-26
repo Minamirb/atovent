d88bccce6f9ae024ed5ed87f6fba7f5db14219f4
yalab <rudeboyjet@gmail.com>
2011-11-26 14:39:37 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atovent::Application.config.secret_token = '90f3465de2b5bb17cea3cbc1785a8d52358302640c0a5178522038b951b30efa0214b8107e5498ec56caef805379858f4f1be0713320da666276fc3248e952b0'
