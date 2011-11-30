711eecdd7aa9ba3820058274a45bacee36e818d3
Goh Matsumoto <urimaro@gmail.com>
2011-11-30 20:04:20 +0900

    change the redirect page which is shown after editting a workshop

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -62,8 +62,10 @@ class WorkshopsController < ApplicationController
 
     respond_to do |format|
       if @workshop.update_attributes(params[:workshop])
-        format.html { redirect_to @workshop, notice: 'Workshop was successfully updated.' }
-        format.json { head :ok }
+        @workshop = Workshop.all
+
+        format.html { redirect_to workshops_url }
+        format.json { render json: @workshops }
       else
         format.html { render action: "edit" }
         format.json { render json: @workshop.errors, status: :unprocessable_entity }
