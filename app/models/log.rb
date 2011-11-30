4c5e98742e9d054ad9c9e94b4d635ccaed532043
yalab <rudeboyjet@gmail.com>
2011-11-30 17:46:12 +0900

    Implement Log.extract_tweets.

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,8 @@
 class Log < ActiveRecord::Base
+  def self.extract_tweets(hashtag)
+    tweets = Twitter.new.search(hashtag)
+    tweets.each do |tweet|
+      Log.create(:id_str => tweet[:id], :text => tweet[:text], :user_id_str => tweet[:user][:id], :user_icon_url => tweet[:user][:profile_image_url], :track => hashtag)
+    end
+  end
 end
--- test/unit/log_test.rb
+++ test/unit/log_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class LogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test ".extract_tweets" do
+    url = Twitter.new.instance_variable_get(:@search_host) + '/search.json?q=%23minamirb'
+    stub_request(:get, url).to_return(:body => fixture_text('minamirb_tweets'))
+    assert_difference 'Log.count', +15 do
+      Log.extract_tweets("#minamirb")
+    end
+  end
 end
