954486e0674ece6d54fb2071491db3c2633a5868
rynqs <cecilcat@gmail.com>
2011-12-01 02:01:05 +0900

    pass test

--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
-  test "access to attributes" do
-    page = Page.new(:title => 'title'
+  test "should get attributes" do
+    page = Page.new(:title => 'Test title', :url => 'http://dummy.co.jp', :type => 'HTML', :host => 'dummy.co.jp', :workshop_id => 1, :log_id => 1)
+    assert_equal 'Test title', page.title
+    assert_equal 'http://dummy.co.jp', page.url
+    assert_equal 'HTML', page.type
+    assert_equal 'dummy.co.jp', page.host
+    assert_equal 1, page.workshop_id
+    assert_equal 1, page.log_id
+  end
 end
