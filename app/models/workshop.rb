f586f6872d3eecd64c81dc7092aee919aa03e0d9
yalab <rudeboyjet@gmail.com>
2011-12-02 23:30:10 +0900

    Add namespace module on Twitter class.

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -2,7 +2,7 @@ class Workshop < ActiveRecord::Base
   has_many :logs
 
   def extract_tweets
-    tweets = Twitter.new.search(hashtag)
+    tweets = Atovent::Twitter.new.search(hashtag)
     tweets.each do |tweet|
       logs.create(:id_str => tweet[:id], :text => tweet[:text], :user_id_str => tweet[:user][:id], :user_screen_name => tweet[:user][:screen_name], :user_icon_url => tweet[:user][:profile_image_url], :track => hashtag)
     end
